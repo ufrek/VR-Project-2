bbc03702034bd9d46090061bc321878a6079ff18
yoAlbireo <robert.ks.email@gmail.com>
2021-03-03 23:50:19 -0500

    Wrong Template Project

--- VRProject2/Assets/Scripts/SpawnPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpawnPoint : MonoBehaviour
-{
-    public Vector3 getPosition => this.gameObject.transform.position;
-}
