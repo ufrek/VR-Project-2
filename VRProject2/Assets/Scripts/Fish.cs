df24e70b5d769fc76eead17f1e3c0754e6647482
yoAlbireo <robert.ks.email@gmail.com>
2021-03-03 23:45:27 -0500

    VR Template Project
    Just Basic Scene Setup

---
+++ VRProject2/Assets/Scripts/Fish.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    bool hasTarget = false;
+    Vector3 target;
+   
+    
+    [SerializeField]
+    float turnSpeed = 2;
+    [SerializeField]
+    float moveSpeed = 5;
+    [SerializeField]
+    AudioClip sBubble;
+
+    bool ishit;
+
+    AudioSource fx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fx = GameObject.FindGameObjectWithTag("FX").GetComponent<AudioSource>();
+        fx.PlayOneShot(sBubble);
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasTarget)
+        {
+            rotateTowards(target);
+            transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed);
+        }
+        
+    }
+
+    public void SetTarget(Vector3 position)
+    {
+        target = position;
+        hasTarget = true;
+
+    }
+
+    
+    protected void rotateTowards(Vector3 to)
+    {
+
+        Quaternion lookRotation =
+            Quaternion.LookRotation((to - transform.position).normalized);
+
+        //over time
+        transform.rotation =
+            Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    public void miss()
+    {
+        StartCoroutine(SelfDestruct());
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Turtle")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(.5f);   //hardcoded, fix when needed
+        Destroy(this.gameObject);
+    }
+
+
+    }
---
+++ VRProject2/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseClick : MonoBehaviour
+{
+    public float rayLength;
+    public GameObject fishPrefab;
+    public LayerMask layerMask;
+
+    [SerializeField]
+    float rayDistance = 700;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, rayLength, layerMask))
+            {
+                if (hit.collider.tag == "Apple")
+                {
+                    Vector3 position = Camera.main.transform.position;
+                    position.z -= 10;
+                    GameObject fish = Instantiate(fishPrefab);
+                    fish.transform.position = position;
+                    fish.GetComponent<Fish>().SetTarget(hit.collider.transform.position);
+                }
+            }
+            else 
+            {
+                Vector3 blankTarget = CastRayToWorld();
+                
+                Vector3 position = Camera.main.transform.position;
+                position.z -= 10;
+                GameObject fish = Instantiate(fishPrefab);
+                fish.transform.position = position;
+                fish.GetComponent<Fish>().SetTarget(blankTarget);
+                fish.GetComponent<Fish>().miss();
+
+
+
+            }
+
+        }
+    }
+
+    Vector3 CastRayToWorld()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 point = ray.origin + (ray.direction * rayDistance);
+        return point;
+    }
+}
