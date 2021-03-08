cbd1f96b736e84fd0051edbab4ad4cdc8987bae2
yoAlbireo <robert.ks.email@gmail.com>
2021-03-08 15:25:18 -0500

    Cleaned up Project Assets and added basic gaze script

---
+++ VR2/Assets/Scripts/ScavengerHuntManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScavengerHuntManager : MonoBehaviour
+{
+    private VRObjectinteract[] huntItems;
+    private int itemCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject mgr = GameObject.FindGameObjectWithTag("HuntMgr");
+        huntItems = mgr.GetComponentsInChildren<VRObjectinteract>();
+        itemCount = huntItems.Length;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
