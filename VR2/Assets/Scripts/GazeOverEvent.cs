990a4514e3a32e40411ddc8aee38e1579c16bc3c
ufrek <robert.ks.email@gmail.com>
2021-03-09 17:37:19 -0500

    Almost Fixed Gazing

---
+++ VR2/Assets/Scripts/GazeOverEvent.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GazeOverEvent : MonoBehaviour
+{
+    [Range(0, 360)]
+    public float maximiumViewingAngle = 30f;
+
+    public UnityEvent OnGazeEnter;
+    public UnityEvent OnGazeExit;
+    public UnityEvent OnGazeClick;
+
+    private bool isHovering = false;
+    private bool doOnce = false;
+    private VRObjectInteract obj;
+
+    void Start()
+    {
+        obj = this.gameObject.GetComponent<VRObjectInteract>();
+    }
+    //called once per frame
+    void Update()
+    {
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 toObject = this.transform.position - Camera.main.transform.position;
+        float angleToObject = Vector3.Angle(forward, toObject);
+        if (angleToObject < maximiumViewingAngle)
+        {
+            Hovering();
+
+            if (Google.XR.Cardboard.Api.IsTriggerPressed || Input.GetMouseButtonDown(0))
+            {
+
+                obj.OnPointerClick();
+            }
+        }
+        else
+        {
+            NotHovering();
+        }
+        
+    }
+
+    private void Hovering()
+    {
+        if (!isHovering)
+        {
+            obj.OnPointerEnter();
+            isHovering = true;
+        }
+        
+    }
+
+    private void NotHovering()
+    {
+        if (isHovering)
+        {
+            obj.OnPointerExit();
+            isHovering = false;
+        }
+    }
+        
+    
+
+
+
+}
